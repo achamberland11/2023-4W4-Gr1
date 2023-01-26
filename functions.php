9a8ea4f1bc010897b9e0de3de786d57afa078abc
Antoine Chamberland <e2147499@cmaisonneuve.qc.ca>
2023-01-26 11:26:18 -0500

    Semaine 01 cours 02. Ajout de add_action pour enfiler style.css dans functions.php (bug possible)

--- functions.php
+++ functions.php
@@ -2,9 +2,12 @@
     /**
      * L'ensemble des fonctions du thème
      */
+    function enfiler_css() {
+        wp_enqueue_style('4W4-Gr2-principal', // Identifier
+            get_template_directory_uri() . '/style.css', // Addresse url de style.css
+            array(), // Définir les dépendances
+            filemtime(get_template_directory() . '/style.css'), // Le calcul de la version du fichier css
+            'all'); // Media
+    }
 
-    wp_enqueue_style('main-styles', // Identifier
-        get_template_directory_uri() . '/css/style.css', // Addresse url de style.css
-        array(), // Définir les dépendances
-        filemtime(get_template_directory() . '/css/style.css'), // Le calcul de la version du fichier css
-        'all'); // Media
+    add_action('wp_enqueue_scripts', 'enfiler_css');
--- index.php
+++ index.php
@@ -1,13 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>4W4</title>
-    <?php wp_head() ?>
-</head>
-<body>
-    <h1>(Bienvenue sur 4W4)</h1>
+<?php
+    /**
+     * index.php est le model par défaut du thème 4W4
+     */
+?>
+
+
+    <?php get_header(); ?>
+    <main>
+        <h1>Bienvenue sur 4W4</h1>
+    </main>
+
+    <?php get_footer(); ?>
+
 </body>
 </html>
