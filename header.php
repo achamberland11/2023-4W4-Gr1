dafbe3ce3f06fe4102d5263175dbda34a43c1f96
Antoine Chamberland <e2147499@cmaisonneuve.qc.ca>
2023-01-31 10:09:49 -0500

    Utilisation des fichiers du prof

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>4W4</title>
+    <title>mon 4W4</title>
     <?php wp_head(); ?>
 </head>
 <body>
