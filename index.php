b67e19e8c7bf72c65b3d03650eaa63b81d5899c5
Antoine Chamberland <e2147499@cmaisonneuve.qc.ca>
2023-02-07 12:04:52 -0500

    S3C1 Modification du style, ajout d'un menu navigation, première intégration du logo

--- index.php
+++ index.php
@@ -7,17 +7,18 @@
 <?php get_header(); ?>
     <main>
         <pre>front-page.php</pre>
-        <h1>Bienvenue sur 4W4</h1>        
-        <?php if(have_posts()):
-            while(have_posts()): the_post(); ?>
-            <article>
-                <a href="<?php the_permalink(); ?>"><h3><?php the_title('<h3>', '<h3>'); ?></h3></a>
-                <?php the_excerpt(); ?>
-                <?php the_permalink(); ?>
-            </article>
-            <?php endwhile; ?>
-        <?php endif; ?>
-        
+        <h1>Bienvenue sur 4W4</h1>
+        <div class="articles">
+            <?php if(have_posts()):
+                while(have_posts()): the_post(); ?>
+                <article>
+                    <a href="<?php the_permalink(); ?>"><h3><?php the_title('<h3>', '<h3>'); ?></h3></a>
+                    <?php the_excerpt(); ?>
+                    <?php the_permalink(); ?>
+                </article>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div>           
     </main>
     
     <?php get_footer(); ?>
