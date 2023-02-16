3c5c52c8173abb5ddbed1d00e8e7a00bd37c5f3e
Antoine Chamberland <e2147499@cmaisonneuve.qc.ca>
2023-02-16 11:45:07 -0500

    S4C2. Ajout de catégories et implémentation de media querry

---
+++ category.php
@@ -0,0 +1,30 @@
+<?php
+    /**
+     * category.php est le modèle par dfaut pour afficher une archive 
+     * d'articles de catégorie spécifique
+     */
+?>
+
+<?php get_header(); ?>
+    <main>
+        <pre>front-page.php</pre>
+        <h1>Bienvenue sur 4W4</h1>    
+        
+        <section class="blocflex">
+            <?php if(have_posts()):
+                while(have_posts()): the_post(); ?>
+                    <article>
+                        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                        <?php //get_the_excerpt(); ?>
+                        <hr>
+                        <?= wp_trim_words(get_the_excerpt(), 10, "[...]"); ?>
+                    </article>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </section>
+        
+    </main>
+    
+    <?php get_footer(); ?>
+</body>
+</html>
