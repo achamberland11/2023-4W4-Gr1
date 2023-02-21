ad0f6e777c5e0f7ac3b2463134e9c3fe3c21b7d6
Antoine Chamberland <e2147499@cmaisonneuve.qc.ca>
2023-02-21 11:56:52 -0500

    Mise à jour des articles et ajout d'une barre de recherche. Modification du menu pour qu'il soit par catégorie

--- front-page.php
+++ front-page.php
@@ -9,16 +9,17 @@
         <pre>front-page.php</pre>
         <h1>Bienvenue sur 4W4</h1>    
         
-        <section class="blocflex">
+        <section class="blocflex">            
+            
             <?php if(have_posts()):
-                while(have_posts()): the_post(); ?>
+                    while(have_posts()): the_post(); ?>
                     <article>
                         <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                         <?php //get_the_excerpt(); ?>
                         <hr>
                         <?= wp_trim_words(get_the_excerpt(), 10, "[...]"); ?>
                     </article>
-                <?php endwhile; ?>
+                    <?php endwhile; ?>
             <?php endif; ?>
         </section>
         
