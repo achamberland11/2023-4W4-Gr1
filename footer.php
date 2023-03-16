26b867d473205f15d6a92a91febf11134c3fccdd
Antoine Chamberland <e2147499@cmaisonneuve.qc.ca>
2023-03-16 11:53:32 -0400

    Premirère intégration du footer

--- footer.php
+++ footer.php
@@ -1,8 +1,15 @@
     <footer class="site__footer">
-    <section class="lien">Lien #1</section>
-    <section class="lien">Lien #2</section>
-    <section class="lien">Lien #3</section>
-</footer>
+        <section class="lien"><?php the_custom_logo() ?></section>
+        
+        <div class="lien">
+                <section class="lien"><a  href="<?= bloginfo('url'); ?>"><?= bloginfo('name'); ?></a></section>
+                <?php  wp_nav_menu(array( 
+                                "menu" => "entete",
+                                "container" => "nav",
+                                "container_class" => "menu__entete"
+                )); ?>
+        </div>
+    </footer>
     <?php wp_footer(); ?>
 
 </body>
