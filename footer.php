fc9ae0a82d44c371c9f1ae4ad1123bf6478ade2a
Antoine Chamberland <e2147499@cmaisonneuve.qc.ca>
2023-03-17 12:30:36 -0400

    Intégration des icones du footer

--- footer.php
+++ footer.php
@@ -13,9 +13,9 @@
         </section>
         <section class="section__footer">
             <ul class="menu__footer">
-                <a href=""><li>Github</li></a>
-                <a href=""><li>LinkedIn</li></a>
-                <a href=""><li>Osama</li></a>
+                <a href="https://cidweb13.sg-host.com/"><li>Github <img src="https://s2.svgbox.net/social.svg?ic=github&color=f0f8ff" width="32" height="32"></li></a>
+                <a href=""><li>Portfolio <img src="https://s2.svgbox.net/hero-solid.svg?ic=folder&color=f0f8ff" width="32" height="32"></li></a>
+                <a href=""><li>Osama <img src="https://s2.svgbox.net/materialui.svg?ic=web&color=f0f8ff" width="32" height="32"></li></a>
             </ul>       
         </section>
     </footer>
