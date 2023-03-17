1046d2ae3f30052af856f6e0a5579cd02d0abfef
Antoine Chamberland <e2147499@cmaisonneuve.qc.ca>
2023-03-17 13:16:27 -0400

    Lien externe footer

--- footer.php
+++ footer.php
@@ -13,9 +13,9 @@
         </section>
         <section class="section__footer">
             <ul class="menu__footer">
-                <a href="https://cidweb13.sg-host.com/"><li>Github <img src="https://s2.svgbox.net/social.svg?ic=github&color=f0f8ff" width="32" height="32"></li></a>
-                <a href=""><li>Portfolio <img src="https://s2.svgbox.net/hero-solid.svg?ic=folder&color=f0f8ff" width="32" height="32"></li></a>
-                <a href=""><li>Osama <img src="https://s2.svgbox.net/materialui.svg?ic=web&color=f0f8ff" width="32" height="32"></li></a>
+                <a href="https://github.com/achamberland11/2023-4W4-Gr1"><li><img src="https://s2.svgbox.net/social.svg?ic=github&color=f0f8ff" width="32" height="32"></li></a>
+                <a href=""><li><img src="https://s2.svgbox.net/hero-solid.svg?ic=folder&color=f0f8ff" width="32" height="32"></li></a>
+                <a href="https://cidweb13.sg-host.com/"><li><img src="https://s2.svgbox.net/materialui.svg?ic=web&color=f0f8ff" width="32" height="32"></li></a>
             </ul>       
         </section>
     </footer>
