0f1ff7c01e8db8e2373b2bf03f95f17d301716bb
Antoine Chamberland <e2147499@cmaisonneuve.qc.ca>
2023-03-17 11:05:32 -0400

    Deuxième intégration du footer et style du footer

--- footer.php
+++ footer.php
@@ -1,16 +1,25 @@
     <footer class="site__footer">
-        <section class="lien"><?php the_custom_logo() ?></section>
-        
-        <div class="lien">
-                <section class="lien"><a  href="<?= bloginfo('url'); ?>"><?= bloginfo('name'); ?></a></section>
-                <?php  wp_nav_menu(array( 
-                                "menu" => "entete",
+        <section class="section__footer">
+            <div class="logo">
+                <?php the_custom_logo() ?>
+            </div>        
+        </section>    
+        <section class="section__footer">
+        <?php  wp_nav_menu(array( 
+                                "menu" => "Footer",
                                 "container" => "nav",
-                                "container_class" => "menu__entete"
-                )); ?>
-        </div>
+                                "container_class" => "menu__footer"
+        )); ?>
+        </section>
+        <section class="section__footer">
+            <ul class="menu__footer">
+                <a href=""><li>Github</li></a>
+                <a href=""><li>LinkedIn</li></a>
+                <a href=""><li>Osama</li></a>
+            </ul>       
+        </section>
     </footer>
     <?php wp_footer(); ?>
 
 </body>
-</html>    
+</html>
