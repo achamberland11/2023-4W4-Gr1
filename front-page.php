4ce4d8bc247273feee6afae85283f3eb37395184
Antoine Chamberland <e2147499@cmaisonneuve.qc.ca>
2023-04-25 11:45:56 -0400

    Intégration page événement

--- front-page.php
+++ front-page.php
@@ -6,17 +6,23 @@
 
 <?php get_header(); ?>
     <main class="site__main">
-        <pre>front-page.php</pre>
         <h1>Bienvenue sur 4W4</h1>
         <section class="blocflex">
+            <section class="menu-evenement-container">
+                <?php wp_nav_menu(array(
+                    "menu" => "evenement",
+                    "container" => "nav",
+                    "container_class" => "menu__evenement"));
+                    ?>
+            </section>
             <?php if(have_posts()):
                 while (have_posts()): the_post(); 
-                    $la_categorie = '4w4';
-                    if (in_category('galerie')){
-                        $la_categorie = 'galerie';}
+                $la_categorie = '4w4';
+                if (in_category('galerie')){
+                    $la_categorie = 'galerie';}
                     get_template_part("template-parts/categorie", $la_categorie);
-               endwhile; 
-             endif; ?>
+                endwhile; 
+            endif; ?>
         </section>
     </main>
 <?php get_footer(); ?>
