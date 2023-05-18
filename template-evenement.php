d263a160e6d9aef9322295dc79c7f21f30d0e4e1
Antoine Chamberland <e2147499@cmaisonneuve.qc.ca>
2023-05-18 15:44:58 -0400

    Intégration du post thumbnail

--- template-evenement.php
+++ template-evenement.php
@@ -9,6 +9,7 @@
     <?php
     if ( have_posts() ) : the_post(); ?>
     <h1><?= get_the_title(); ?></h1>
+    <?php the_post_thumbnail('medium'); ?>
     <?php the_content();?>
     <p>L'adresse de l'evénement :<?php the_field('adresse'); ?></p>
     <p>La date et l'heure de l'événement : <?php the_field('date'); ?></p>
